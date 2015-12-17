d9a36d4b529bdeb8552ff1bdc19e2f4af5fd9430
Matthias Wolf <matthias@sushinara.net>
2015-12-17 13:57:16 +0100

    Update configs.
    * Add data config
    * Comment out saving everything

--- test/analyze_1x1_tp.py
+++ test/analyze_1x1_tp.py
@@ -14,14 +14,25 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
 process.source = cms.Source("PoolSource",
-        fileNames = cms.untracked.vstring('/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/0C2BBF49-3387-E511-ADD5-3417EBE6459A.root')
+        fileNames = cms.untracked.vstring(
+                '/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/0C2BBF49-3387-E511-ADD5-3417EBE6459A.root',
+                '/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/200C7831-3987-E511-944F-00266CF20468.root',
+                '/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/3849AE0A-3487-E511-90EF-00266CFAE228.root',
+                '/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/40550625-3087-E511-A4C4-00266CFAE764.root',
+                '/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/500B67F4-3487-E511-B068-00266CF2506C.root',
+                '/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/5267F176-3887-E511-927F-00266CFADEC0.root',
+                '/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/5EF74DD0-3187-E511-B2BF-00266CF9BEF8.root',
+                '/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/6C09D335-4087-E511-AFE5-00266CF9B9F0.root',
+                '/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/6EBBD119-3187-E511-ACB3-00266CF9B828.root',
+                '/store/relval/CMSSW_8_0_0_pre1/RelValHydjetQ_MinBias_5020GeV/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_HeavyIon_v11_resub-v1/00000/86C86721-3787-E511-A445-00A0D1EE271C.root',
+        )
 )
 
-process.out = cms.OutputModule( "PoolOutputModule",
-        fileName = cms.untracked.string("output.root"),
-        outputCommands = cms.untracked.vstring( 'keep *' )
-)
-process.end = cms.EndPath(process.out)
+# process.out = cms.OutputModule( "PoolOutputModule",
+#         fileName = cms.untracked.string("output.root"),
+#         outputCommands = cms.untracked.vstring( 'keep *' )
+# )
+# process.end = cms.EndPath(process.out)
 
 process.load('L1Trigger.RegionalCaloTrigger.rctDigis_cfi')
 process.rctDigis.hcalDigis = cms.VInputTag(cms.InputTag("simHcalTriggerPrimitiveDigis"))
