a53e2ecad52312016b195ac71977e3adf4d1d54c
Matthias Wolf <matthias@sushinara.net>
2016-01-27 12:59:12 +0100

    Add a configuration testing repacking of 1x1 TP.

---
+++ test/analyze_1x1_tp_repack_test.py
@@ -0,0 +1,98 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HFCALIB")
+
+## Import of standard configurations
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_v0'
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+
+process.source = cms.Source("PoolSource",
+        fileNames = cms.untracked.vstring(
+
+                '/store/relval/CMSSW_7_6_0/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_asymptotic_v11-v1/00000/4431031E-9E7F-E511-9F42-0025905938A4.root',
+                '/store/relval/CMSSW_7_6_0/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_asymptotic_v11-v1/00000/4C462F65-9F7F-E511-972A-0026189438A9.root',
+                '/store/relval/CMSSW_7_6_0/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_asymptotic_v11-v1/00000/703E7EAB-9D7F-E511-B886-003048FFCBFC.root',
+                '/store/relval/CMSSW_7_6_0/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_asymptotic_v11-v1/00000/8AF07AAB-9D7F-E511-B8B4-003048FFCBFC.root',
+                '/store/relval/CMSSW_7_6_0/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_asymptotic_v11-v1/00000/962BEF7C-9D7F-E511-A2BB-0025905B85AA.root',
+                '/store/relval/CMSSW_7_6_0/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_asymptotic_v11-v1/00000/C409A519-9E7F-E511-BD4C-0025905B8590.root',
+                '/store/relval/CMSSW_7_6_0/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_asymptotic_v11-v1/00000/E8D41D6A-9F7F-E511-A10A-003048FFD740.root',
+                '/store/relval/CMSSW_7_6_0/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/76X_mcRun2_asymptotic_v11-v1/00000/EE048767-9E7F-E511-B1AA-0025905B8606.root',
+
+        )
+)
+
+# process.out = cms.OutputModule( "PoolOutputModule",
+#         fileName = cms.untracked.string("output.root"),
+#         outputCommands = cms.untracked.vstring( 'keep *' )
+# )
+# process.end = cms.EndPath(process.out)
+
+process.load('L1Trigger.RegionalCaloTrigger.rctDigis_cfi')
+process.rctDigis.hcalDigis = cms.VInputTag(cms.InputTag("simHcalTriggerPrimitiveDigis"))
+
+process.load("Geometry.HcalCommonData.testPhase0GeometryXML_cfi")
+process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
+process.load("Configuration.Geometry.GeometryReco_cff")
+
+process.load('SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff')
+process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag( cms.InputTag('simHcalUnsuppressedDigis'), cms.InputTag('simHcalUnsuppressedDigis') )
+# process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag( cms.InputTag('simHcalDigis'), cms.InputTag('simHcalDigis') )
+process.simHcalTriggerPrimitiveDigis.FrontEndFormatError = cms.bool(False)
+
+# process.es_ascii = cms.ESSource("HcalTextCalibrations",
+#     input = cms.VPSet(
+#         cms.PSet(
+#             object = cms.string('LutMetadata'),
+#             # full path: /afs/cern.ch/user/a/akhukhun/public/HF1x1TPs/LutMetadata_1x1.txt
+#             file = cms.FileInPath('LutMetadata_1x1.txt')
+#         )
+#     )
+# )
+# process.es_prefer_es_ascii = cms.ESPrefer("HcalTextCalibrations", "es_ascii")
+
+process.es_pool = cms.ESSource("PoolDBESSource",
+     process.CondDBSetup,
+     timetype = cms.string('runnumber'),
+     toGet = cms.VPSet(
+         cms.PSet(record = cms.string("HcalLutMetadataRcd"),
+             tag = cms.string("HcalLutMetadata_HFTP_1x1")
+             )
+         ),
+     connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+     authenticationMethod = cms.untracked.uint32(0)
+     )
+process.es_prefer_es_pool = cms.ESPrefer( "PoolDBESSource", "es_pool" )
+
+# process.load('CalibCalorimetry.HcalPlugins.Hcal_Conditions_forGlobalTag_cff')
+# process.es_hardcode.toGet.append("LutMetadata")
+
+process.TFileService = cms.Service("TFileService",
+        closeFileFast = cms.untracked.bool(True),
+        fileName = cms.string('analyze.root'))
+
+process.load("EventFilter.HcalRawToDigi.HcalDigiToRaw_cfi")
+process.load("EventFilter.HcalRawToDigi.HcalRawToDigi_cfi")
+
+process.hcalRawData.TRIG = cms.untracked.InputTag("simHcalTriggerPrimitiveDigis", "" , "HFCALIB")
+process.hcalDigis.InputLabel = cms.InputTag("hcalRawData")
+
+process.analyze = cms.EDAnalyzer("AnalyzeTP",
+        triggerPrimitives = cms.InputTag("hcalDigis", "" , "HFCALIB"))
+process.analyzeOld = cms.EDAnalyzer("AnalyzeTP",
+        triggerPrimitives = cms.InputTag("simHcalTriggerPrimitiveDigis", "" , "HLT"))
+
+process.p = cms.Path(
+        process.simHcalTriggerPrimitiveDigis
+        * process.hcalRawData
+        * process.hcalDigis
+        * process.analyze
+        * process.analyzeOld
+)
+
+# print process.dumpPython()
