8026f2a0d4b865f96d4abaa5ecaf362619c70b60
Matthias Wolf <matthias.wolf@cern.ch>
2016-09-08 13:13:48 +0200

    Add comparisons.

--- test/analyze_2017_tp.py
+++ test/analyze_2017_tp.py
@@ -14,7 +14,8 @@ process.options = cms.untracked.PSet(wantSummary=cms.untracked.bool(True))
 process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(1000))
 
 process.source = cms.Source("PoolSource",
-                            fileNames=cms.untracked.vstring('file:step2.root'))
+                            fileNames=cms.untracked.vstring('file:step3.root'),
+                            secondaryFileNames=cms.untracked.vstring('file:step2.root'))
 
 # process.out = cms.OutputModule( "PoolOutputModule",
 #         fileName = cms.untracked.string("output.root"),
@@ -26,31 +27,13 @@ process.load('L1Trigger.RegionalCaloTrigger.rctDigis_cfi')
 process.rctDigis.hcalDigis = cms.VInputTag(cms.InputTag("simHcalTriggerPrimitiveDigis"))
 
 process.load('SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff')
-process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag(cms.InputTag('simHcalUnsuppressedDigis'), cms.InputTag('simHcalUnsuppressedDigis'))
+process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag(
+    cms.InputTag('simHcalUnsuppressedDigis'), cms.InputTag('simHcalUnsuppressedDigis'))
 # process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag( cms.InputTag('simHcalDigis'), cms.InputTag('simHcalDigis') )
 process.simHcalTriggerPrimitiveDigis.FrontEndFormatError = cms.bool(False)
 
-process.load("Configuration.Geometry.GeometryExtended2016Reco_cff")
-from CondCore.CondDB.CondDB_cfi import CondDB
-
-process.CondDBSetup = CondDB.clone()
-delattr(process.CondDBSetup, 'connect')
-
-# process.es_pool = cms.ESSource("PoolDBESSource",
-#      process.CondDBSetup,
-#      timetype = cms.string('runnumber'),
-#      toGet = cms.VPSet(
-#          cms.PSet(record = cms.string("HcalLutMetadataRcd"),
-#              tag = cms.string("HcalLutMetadata_HFTP_1x1")
-#              ),
-#          cms.PSet(record = cms.string("HcalElectronicsMapRcd"),
-#              tag = cms.string("HcalElectronicsMap_HFTP_1x1")
-#              )
-#          ),
-#      connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
-#      authenticationMethod = cms.untracked.uint32(0)
-#      )
-# process.es_prefer_es_pool = cms.ESPrefer( "PoolDBESSource", "es_pool" )
+process.load('Configuration.Geometry.GeometryExtended2017devReco_cff')
+process.load('Configuration.Geometry.GeometryExtended2017dev_cff')
 
 from SLHCUpgradeSimulations.Configuration.HCalCustoms import customise_Hcal2017Full
 customise_Hcal2017Full(process)
@@ -61,7 +44,17 @@ process.TFileService = cms.Service("TFileService",
 
 process.analyze = cms.EDAnalyzer("AnalyzeTP",
                                  triggerPrimitives=cms.InputTag("simHcalTriggerPrimitiveDigis", "", ""))
+process.chainplotter = cms.EDAnalyzer("HcalCompareLegacyChains",
+                                      triggerPrimitives=cms.InputTag(
+                                          'simHcalTriggerPrimitiveDigis', '', ''),
+                                      recHits=cms.VInputTag('hbheprereco', 'hfreco'),
+                                      dataFrames=cms.VInputTag(
+                                          cms.InputTag("hcalDigis", "", ""),
+                                          cms.InputTag("hcalDigis", "", "")
+                                      ),
+                                      swapIphi=cms.bool(False)
+                                      )
 
-process.p = cms.Path(process.analyze)
+process.p = cms.Path(process.analyze * process.chainplotter)
 
 # print process.dumpPython()
