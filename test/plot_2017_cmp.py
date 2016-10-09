90d56e0ab59e757805c73f521c836e04cab8fe3b
Matthias Wolf <matthias.wolf@cern.ch>
2016-10-09 23:49:48 +0200

    Provide diagonal in 2017 comparisons.

--- test/plot_2017_cmp.py
+++ test/plot_2017_cmp.py
@@ -6,6 +6,9 @@ r.gStyle.SetOptStat(0)
 
 infile, outfile = sys.argv[1:]
 
+f1 = r.TF1("f1", "x", 0, 200)
+f1.SetLineColor(r.kRed)
+
 f = r.TFile(infile)
 tps = f.Get("chainplotter/matches")
 
@@ -14,12 +17,15 @@ c.SetLogz(True)
 c.SetRightMargin(c.GetRightMargin() * 1.5)
 c.SaveAs(outfile + '[')
 tps.Draw("RH_energy:TP_energy>>cmphb(100, 0, 200, 100, 0, 200)", "abs(ieta) <= 16", "COLZ")
+f1.Draw("same")
 r.gDirectory.Get("cmphb").SetTitle("HB energy comparison;TP E_{T};RH E_{T}")
 c.SaveAs(outfile)
 tps.Draw("RH_energy:TP_energy>>cmphe(100, 0, 100, 100, 0, 100)", "abs(ieta) > 16 && abs(ieta) < 29", "COLZ")
+f1.Draw("same")
 r.gDirectory.Get("cmphe").SetTitle("HE energy comparison;TP E_{T};RH E_{T}")
 c.SaveAs(outfile)
 tps.Draw("RH_energy:TP_energy/0.7>>cmphf(80, 0, 80, 80, 0, 80)", "abs(ieta) > 29", "COLZ")
+f1.Draw("same")
 r.gDirectory.Get("cmphf").SetTitle("HF energy comparison;TP E_{T};RH E_{T} / 0.7")
 c.SaveAs(outfile)
 c.SaveAs(outfile + ']')
