a22acc21b7061589303792ffdbc75a9cc6cc658d
Matthias Wolf <matthias.wolf@cern.ch>
2016-10-13 12:50:26 +0200

    Configurations for 2017 wedge investigation.

--- test/analyze_2017_vanilla_tp.py
+++ test/analyze_2017_vanilla_tp.py
@@ -3,14 +3,14 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.AlCa.GlobalTag import GlobalTag
 from Configuration.StandardSequences.Eras import eras
 
-process = cms.Process('PLOT', eras.Run2_2017_HCALdev)
+process = cms.Process('PLOT', eras.Run2_2017)
 
 # Import of standard configurations
 process.load('FWCore/MessageService/MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2017_hcaldev', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2017_realistic', '')
 
 process.options = cms.untracked.PSet(wantSummary=cms.untracked.bool(True))
 process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(1000))
@@ -34,11 +34,7 @@ process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag(
 # process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag( cms.InputTag('simHcalDigis'), cms.InputTag('simHcalDigis') )
 process.simHcalTriggerPrimitiveDigis.FrontEndFormatError = cms.bool(False)
 
-process.load('Configuration.Geometry.GeometryExtended2017devReco_cff')
-
-# customise_Hcal2017Full(process)
-process.simHcalTriggerPrimitiveDigis.upgradeHE = cms.bool(True)
-process.simHcalTriggerPrimitiveDigis.upgradeHF = cms.bool(True)
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 
 process.TFileService = cms.Service("TFileService",
                                    closeFileFast=cms.untracked.bool(True),
