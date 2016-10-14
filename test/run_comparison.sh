0a205ad37be5118be40e1ad72e52b854869fce78
Matthias Wolf <matthias.wolf@cern.ch>
2016-10-14 14:18:03 +0200

    Fix input filenames.

--- test/run_comparison.sh
+++ test/run_comparison.sh
@@ -17,8 +17,8 @@ run_ttbar() {
    cd $dir
 
    cmsDriver.py TTbar_13TeV_TuneCUETP8M1_cfi  --conditions auto:$conditions -n $events --era $era --eventcontent FEVTDEBUG --relval 9000,50 -s GEN,SIM --datatier GEN-SIM --beamspot Realistic50ns13TeVCollision --fileout=step1.root $geometry &> step1.log
-   cmsDriver.py step2  --conditions auto:$conditions -s DIGI:pdigi_valid,L1,DIGI2RAW,HLT:@fake --datatier GEN-SIM-DIGI-RAW -n $events $geometry --era $era --eventcontent FEVTDEBUGHLT --fileout=step2.root --filein=step1.root &> step2.log
-   cmsDriver.py step3  --conditions auto:$conditions -n $events --era $era --eventcontent RECOSIM,MINIAODSIM,DQM --runUnscheduled  -s RAW2DIGI,L1Reco,RECO,EI,PAT,VALIDATION:@standardValidation+@miniAODValidation,DQM:@standardDQM+@miniAODDQM --datatier GEN-SIM-RECO,MINIAODSIM,DQMIO --fileout=step3.root --filein=step2.root $geometry &> step3.log
+   cmsDriver.py step2  --conditions auto:$conditions -s DIGI:pdigi_valid,L1,DIGI2RAW,HLT:@fake --datatier GEN-SIM-DIGI-RAW -n $events $geometry --era $era --eventcontent FEVTDEBUGHLT --fileout=step2.root --filein=file:step1.root &> step2.log
+   cmsDriver.py step3  --conditions auto:$conditions -n $events --era $era --eventcontent RECOSIM,MINIAODSIM,DQM --runUnscheduled  -s RAW2DIGI,L1Reco,RECO,EI,PAT,VALIDATION:@standardValidation+@miniAODValidation,DQM:@standardDQM+@miniAODDQM --datatier GEN-SIM-RECO,MINIAODSIM,DQMIO --fileout=step3.root --filein=file:step2.root $geometry &> step3.log
 }
 
 run_ttbar 2017_HCALdev phase1_2017_hcaldev Run2_2017_HCALdev Extended2017dev &
